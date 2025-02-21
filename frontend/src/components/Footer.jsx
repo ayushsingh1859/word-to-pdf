4a22cbf75d58f0fb349e416072d7a439ae47d8f2
Ayush Singh <ayushsingh18777@gmail.com>
2025-02-22 00:03:56 +0530

    Designing completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <div className='item-centre justify-center'>
+        <hr className=' border-black' />
+        <h1 className='text-center py-3 text-sm'>
+            Copyright @ 2025 - All rights reserved by Ayush 
+        </h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default Footer
